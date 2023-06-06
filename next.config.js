bbd9f0e3160ad98c18735f0d4d21b3672803be8b
aherbez <aherbez@gmail.com>
2023-06-06 01:35:09 -0700

    deploy v3

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+  }
+  
+  module.exports = {
+    webpack: function(config) {
+      config.module.rules.push({
+        test: /\.md$/,
+        use: 'raw-loader',
+      })
+      return config
+    }
+  }
